6c7b3e02f5aafde13010683b82fb77a84784e7f1
AngryCarrot789 <32274404+AngryCarrot789@users.noreply.github.com>
2025-09-30 23:19:36 +0100

    Update task sequencer operations context menu

--- MemEngine360/Sequencing/Contexts/OperationsContextRegistry.cs
+++ MemEngine360/Sequencing/Contexts/OperationsContextRegistry.cs
@@ -19,54 +19,83 @@
 
 using MemEngine360.Sequencing.View;
 using PFXToolKitUI.AdvancedMenuService;
+using PFXToolKitUI.Interactivity.Contexts;
 
 namespace MemEngine360.Sequencing.Contexts;
 
 public static class OperationsContextRegistry {
-    public static readonly ContextRegistry Registry = new ContextRegistry("Operation");
+    private static readonly DataKey<BaseSequenceOperation> CurrentOperationDataKey = DataKeys.Create<BaseSequenceOperation>(nameof(OperationsContextRegistry) + "_internal_" + nameof(CurrentOperationDataKey));
     
+    public static readonly ContextRegistry Registry = new ContextRegistry("Operation");
+
     static OperationsContextRegistry() {
         FixedContextGroup actions = Registry.GetFixedGroup("operations");
         actions.AddCommand("commands.sequencer.DuplicateOperationsCommand", "Duplicate");
-        actions.AddDynamicSubGroup((group, ctx, items) => {
-            if (!TaskSequenceManager.DataKey.TryGetContext(ctx, out TaskSequenceManager? ui)) {
-                return;
+
+        CommandContextEntry entry1 = actions.AddCommand("commands.sequencer.ToggleOperationEnabledCommand", "Toggle Enabled");
+        entry1.AddContextChangeHandler(TaskSequenceManager.DataKey, (entry, oldManager, newManager) => {
+            if (oldManager != null)
+                TaskSequenceManagerViewState.GetInstance(oldManager).PrimarySelectedSequenceChanged -= OnPrimarySelectedSequenceChanged;
+            if (newManager != null)
+                TaskSequenceManagerViewState.GetInstance(newManager).PrimarySelectedSequenceChanged += OnPrimarySelectedSequenceChanged;
+
+            OnSelectionChanged(newManager != null ? TaskSequenceManagerViewState.GetInstance(newManager) : null);
+            return;
+
+            void OnPrimarySelectedSequenceChanged(TaskSequenceManagerViewState sender, TaskSequence? oldSeq, TaskSequence? newSeq) {
+                OnSelectionChanged(sender);
             }
 
-            TaskSequenceManagerViewState state = TaskSequenceManagerViewState.GetInstance(ui);
-            if (state.SelectedOperations != null && state.SelectedOperations.Count > 0) {
-                if (state.SelectedOperations.Count == 1) {
-                    BaseSequenceOperation item = state.SelectedOperations[0];
-                    items.Add(new CommandContextEntry("commands.sequencer.ToggleOperationEnabledCommand", item.IsEnabled ? "Disable" : "Enable"));
+            void OnSelectionChanged(TaskSequenceManagerViewState? sender) {
+                if (CurrentOperationDataKey.TryGetContext(entry.UserContext, out BaseSequenceOperation? currentOperation)) {
+                    entry.UserContext.Remove(CurrentOperationDataKey);
+                    entry.DisplayName = "Toggle Enabled";
+                    entry.IsCheckedFunction = null;
+                    currentOperation.IsEnabledChanged -= OnIsOperationEnabledChanged;
                 }
-                else {
-                    items.Add(new CommandContextEntry("commands.sequencer.ToggleOperationEnabledCommand", "Toggle Enabled"));
+
+                if (sender != null && sender.SelectedOperations != null && sender.SelectedOperations.Count == 1) {
+                    BaseSequenceOperation newOp = sender.SelectedOperations[0];
+                    entry.UserContext.Set(CurrentOperationDataKey, newOp);
+                    entry.DisplayName = "Is Enabled";
+                    entry.IsCheckedFunction = static e => CurrentOperationDataKey.GetContext(e.UserContext)!.IsEnabled;
+                    newOp.IsEnabledChanged += OnIsOperationEnabledChanged;
                 }
+                
+                return;
+
+                void OnIsOperationEnabledChanged(BaseSequenceOperation op) => entry.RaiseIsCheckedChanged();
             }
         });
-        
+
+        // actions.AddDynamicSubGroup((group, ctx, items) => {
+        //     if (!TaskSequenceManager.DataKey.TryGetContext(ctx, out TaskSequenceManager? ui)) {
+        //         return;
+        //     }
+        //
+        //     TaskSequenceManagerViewState state = TaskSequenceManagerViewState.GetInstance(ui);
+        //     if (state.SelectedOperations != null && state.SelectedOperations.Count > 0) {
+        //         if (state.SelectedOperations.Count == 1) {
+        //             BaseSequenceOperation item = state.SelectedOperations[0];
+        //             items.Add(new CommandContextEntry("commands.sequencer.ToggleOperationEnabledCommand", item.IsEnabled ? "Disable" : "Enable"));
+        //         }
+        //         else {
+        //             items.Add(new CommandContextEntry("commands.sequencer.ToggleOperationEnabledCommand", "Toggle Enabled"));
+        //         }
+        //     }
+        // });
+
         CommandContextEntry entry = actions.AddCommand("commands.sequencer.ToggleOperationConditionBehaviourCommand", "Skip when conditions not met", "Skips over this operation when conditions not met, otherwise, wait until they are met");
+        entry.AddIsCheckedChangeUpdaterForEvent(BaseSequenceOperation.DataKey, nameof(BaseSequenceOperation.ConditionBehaviourChanged));
         entry.IsCheckedFunction = e => {
             if (e.CapturedContext != null && BaseSequenceOperation.DataKey.TryGetContext(e.CapturedContext, out BaseSequenceOperation? operation)) {
                 return operation.ConditionBehaviour == OperationConditionBehaviour.Skip;
             }
-            
+
             return false;
         };
 
-        entry.AddContextChangeHandler(BaseSequenceOperation.DataKey, (e, oldUI, newUI) => {
-            if (oldUI != null)
-                oldUI.ConditionBehaviourChanged -= OperationOnConditionBehaviourChanged;
-            if (newUI != null)
-                newUI.ConditionBehaviourChanged += OperationOnConditionBehaviourChanged;
-        });
-
         actions.AddSeparator();
         actions.AddCommand("commands.sequencer.DeleteOperationSelectionCommand", "Delete");
-        return;
-
-        void OperationOnConditionBehaviourChanged(BaseSequenceOperation sender) {
-            entry.RaiseIsCheckedChanged();
-        }
     }
 }
